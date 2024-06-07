a8600c62d1655f8ea6a70a7d6476a78458c9fdda
agree0002 <163145323+agree0002@users.noreply.github.com>
2024-06-07 23:11:18 +0900

    Title: a/b 출력, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240607/a/b 출력/a-divide-b.py
+++ 240607/a/b 출력/a-divide-b.py
@@ -1,9 +1,10 @@
 n=input().split()
 a=int(n[0])
 b=int(n[1])
-c=a/b
-print(c,end='')
-c-=int(c)
-l=len(str(c))
-for _ in range(22-l):
-    print(0, end='')
+print(int(a/b), end='')
+print('.',end='')
+for i in range(1, 21):
+    if (a/b) * (10**i) % 10 != 0:
+        print(int((a/b) * (10**i) % 10), end='')
+    else:
+        print(0,end='')
