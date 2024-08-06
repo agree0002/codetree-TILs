2e7db0a057c425c4f8fcd192733f394c8c411e2e
agree0002 <163145323+agree0002@users.noreply.github.com>
2024-08-06 19:46:44 +0900

    Title: 최고의 33위치, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=[]
+maxc=0
+for _ in range(n):
+    a.append(list(map(int,input().split())))
+
+for i in range(3):
+    for j in range(3):
+        c=0
+        if i+2 >= n or j+2 >= n:
+            continue
+        for k in range(i,i+3):
+            for s in range(j,j+3):
+                c+=a[k][s]
+        maxc=max(maxc,c)
+
+print(maxc)
