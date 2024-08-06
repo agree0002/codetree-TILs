f626f496e4ab6b3303a8990e205bd64f00c77545
agree0002 <163145323+agree0002@users.noreply.github.com>
2024-08-06 20:29:26 +0900

    Title: 행복한 수열의 개수, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,26 +3,29 @@ a=[]
 c=0
 for _ in range(n):
     a.append(list(map(int,input().split())))
-    
-for i in a:
-    x=1
-    for j in range(1,n):
-        if i[j]==i[j-1]:
-            x+=1
-        else:
-            x=1
-        if x>=m:
-            c+=1
-            break
 
-for j in range(n):
-    x=1
-    for i in range(1,n):
-        if a[i][j]==a[i-1][j]:
-            x+=1
-        else:
-            x=1
-        if x>=m:
-            c+=1
-            break
-print(c)
+if n==1:
+    print(2)
+else:
+    for i in a:
+        x=1
+        for j in range(1,n):
+            if i[j]==i[j-1]:
+                x+=1
+            else:
+                x=1
+            if x>=m:
+                c+=1
+                break
+
+    for j in range(n):
+        x=1
+        for i in range(1,n):
+            if a[i][j]==a[i-1][j]:
+                x+=1
+            else:
+                x=1
+            if x>=m:
+                c+=1
+                break
+    print(c)
