7e3639010adb86357f81ca05dd2e98d3ec333dfb
agree0002 <163145323+agree0002@users.noreply.github.com>
2024-08-17 21:19:27 +0900

    Title: 1차원 폭발 게임, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240817/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240817/1차원 폭발 게임/The-1D-bomb-game.py
@@ -4,35 +4,38 @@ input=sys.stdin.readline
 n,m=map(int,input().split())
 l=[int(input()) for _ in range(n)]
 
-while 1:
-    b=False
-    tl=[]
-    for i in range(1, len(l)):
-        if not b:
-            tl.append(i-1)
-        if l[i-1] == l[i]:
-            tl.append(i)
-            b=True
-            if i==len(l)-1:
+if m==1:
+    print(0)
+else:
+    while 1:
+        b=False
+        tl=[]
+        for i in range(1, len(l)):
+            if not b:
+                tl.append(i-1)
+            if l[i-1] == l[i]:
+                tl.append(i)
+                b=True
+                if i==len(l)-1:
+                    if len(tl) >= m:
+                        for j in tl:
+                            l[j]=0
+            else:
                 if len(tl) >= m:
                     for j in tl:
                         l[j]=0
-        else:
-            if len(tl) >= m:
-                for j in tl:
-                    l[j]=0
-            tl=[]
-            b=False
+                tl=[]
+                b=False
 
-    if 0 not in l:
-        break
+        if 0 not in l:
+            break
 
-    tl2=[]
-    for k in l:
-        if k != 0:
-            tl2.append(k)
-    l=tl2
+        tl2=[]
+        for k in l:
+            if k != 0:
+                tl2.append(k)
+        l=tl2
 
-print(len(l))
-for i in l:
-    print(i)
+    print(len(l))
+    for i in l:
+        print(i)
